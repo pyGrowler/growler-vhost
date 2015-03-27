244c4ceaa3f1163fd375c519afc2e29457c9ed28
Andrew Kubera <andrewkubera@gmail.com>
2015-03-27 16:11:44 +0100

    Formatting fix.

--- setup.py
+++ setup.py
@@ -17,9 +17,8 @@ implementation of the service using the _Growler_ microframework to handle the
 incoming request. As with everything Growler, all events are asynchronous, and
 handled when-needed.
 
-This package comes as a binary, which can be run in the form of
-`growler-vhost -c <config_file>`, specifying the path to the config file to use
-as parameters.
+This package comes as a binary, which can be run in the form of `growler-vhost
+-c <config_file>`, specifying the path to the config file to use as parameters.
 
 I have a hope that optimizations can be made such that forwarding the request to
 another growler server takes (almost) no extra resources. As the request can
