accccc4a646a380c68829de79e85490918a273ef
Andrew Kubera <andrewkubera@gmail.com>
2015-06-16 21:26:44 -0400

    The file setup.py uses the REQUIRES variable and no longer sets long_description explicitly

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # setup.py
 #
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 import growler_vhost
 
@@ -10,21 +10,6 @@ NAME = "growler_vhost"
 
 desc = """The Growler vhost server acts as forwarding agent for HTTP requests to
 multiple domains hosted from a single server."""
-
-description = """The Growler vhost server acts as forwarding agent for HTTP
-requests to multiple domains hosted from a single server. This is an
-implementation of the service using the _Growler_ microframework to handle the
-incoming request. As with everything Growler, all events are asynchronous, and
-handled when-needed.
-
-This package comes as a binary, which can be run in the form of `growler-vhost
--c <config_file>`, specifying the path to the config file to use as parameters.
-
-I have a hope that optimizations can be made such that forwarding the request to
-another growler server takes (almost) no extra resources. As the request can
-easily be parsed once into the format that the growler application already uses.
-We are a long way from that, though.
-"""
 
 REQUIRES = [
     'growler'
@@ -55,8 +40,7 @@ setup(
     url=growler_vhost.__version__,
     author_email=growler_vhost.__contact__,
     description=desc,
-    long_description=description,
     classifiers=CLASSIFIERS
-    install_requires = ['growler'],
-    packages = ['growler_vhost']
+    install_requires=REQUIRES,
+    packages=find_packages(exclude=['tests'])
 )
