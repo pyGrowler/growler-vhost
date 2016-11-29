b1bbba58fb4651d31812bdddb104f17cc7e4c1d2
Andrew Kubera <andrewkubera@gmail.com>
2016-11-29 18:41:06 -0500

    __meta__ file for storing project metadata

--- growler_vhost/__init__.py
+++ growler_vhost/__init__.py
@@ -2,7 +2,10 @@
 # growler_vhost/__init__.py
 #
 
-__version__ = '0.0.0'
-__author__  = 'Andrew Kubera'
-__contact__ = 'andrew.kubera@gmail.com'
-__license__ = 'Apache v2.0'
+from .__meta__ import (
+    version as __version__,
+    date as __date__,
+    author as __author__,
+    license as __license__,
+    author_email as __contact__,
+)
---
+++ growler_vhost/__meta__.py
@@ -0,0 +1,17 @@
+#
+# growler_vhost/__meta__.py
+#
+
+description = """
+An HTTP request-forwarding agent for Growler servers hosting multiple
+domains on a single machine.
+"""
+
+version_info = (0, 0, 1)
+version = '.'.join(map(str, version_info))
+
+date = 'Nov 29, 2016'
+
+author  = 'Andrew Kubera'
+author_email = 'andrew.kubera@gmail.com'
+license = 'Apache v2.0'
