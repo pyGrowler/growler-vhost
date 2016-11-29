39608c0f3c29247efb913d9456064911b6f601a5
Andrew Kubera <andrewkubera@gmail.com>
2016-11-29 18:56:08 -0500

    setup.py: Updated to read information from __meta__.py

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
+#!/usr/bin/env python
 #
 # setup.py
 #
 
-from setuptools import setup, find_packages
-from glob import glob
+from setuptools import setup
+from importlib.machinery import SourceFileLoader
 
-import growler_vhost
 
-NAME = "growler_vhost"
+metadata = SourceFileLoader('metadata', 'growler_vhost/__meta__.py').load_module()
 
-desc = """The Growler vhost server acts as forwarding agent for HTTP requests to
-multiple domains hosted from a single server."""
+NAME = "growler-vhost"
+
+PACKAGES = [
+    'growler_vhost',
+]
 
 REQUIRES = [
     'growler'
@@ -18,16 +21,19 @@ REQUIRES = [
 
 KEYWORDS = [
     'vhost',
-    'virtual server'
+    'virtual host',
+    'virtual server',
+    'reverse proxy',
 ]
 
 CLASSIFIERS = [
     "Development Status :: 2 - Pre-Alpha",
+    "Environment :: Web Environment",
     # "Framework :: Growler",
     "License :: OSI Approved :: Apache Software License",
     "Programming Language :: Python",
     "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
     "Topic :: Internet :: WWW/HTTP",
     "Natural Language :: English"
 ],
@@ -35,14 +41,12 @@ CLASSIFIERS = [
 
 setup(
     name=NAME,
-    version=growler_vhost.__version__,
-    author=growler_vhost.__author__,
-    license=growler_vhost.__license__,
-    url=growler_vhost.__version__,
-    author_email=growler_vhost.__contact__,
-    description=desc,
-    classifiers=CLASSIFIERS
+    version=metadata.version,
+    author=metadata.author,
+    license=metadata.license,
+    author_email=metadata.author_email,
+    description=metadata.description,
+    classifiers=CLASSIFIERS,
     install_requires=REQUIRES,
-    packages=find_packages(exclude=['tests']),
-    scripts=glob("scripts/*")
+    packages=PACKAGES,
 )
